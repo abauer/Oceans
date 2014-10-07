bc87d9267edd3c1858b5e42dfac720fd3a9bca79
Anthony <ambauer@utexas.edu>
2014-10-06 21:48:02 -0500

    Blank Window popup
    Its a start

--- src/Oceans.java
+++ src/Oceans.java
@@ -1,8 +1,18 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.*;
+
 
 public class Oceans {
 	
 	public static void main(String args[]){
-		
+		JWindow frame = new JWindow();
+		frame.setVisible(true);
+		frame.setSize(500, 500);
+//		Graphics g = frame.getGraphics();
+//		g.setColor(Color.CYAN);
+//		g.fillRect(50, 50, 100, 200);		
 	}
 	
 }
