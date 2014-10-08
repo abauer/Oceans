9c6cbb491b9b04ecadfd285a913dc64002d48ad2
Anthony <ambauer@utexas.edu>
2014-10-08 14:40:14 -0500

    Smoothed out terrain (hopefully)

--- src/com/abauer/oceans/window/Game.java
+++ src/com/abauer/oceans/window/Game.java
@@ -30,28 +30,28 @@ public class Game extends JComponent implements MouseListener{
 		//2048x2048 (+1)
 	
 		value[0][0]=value[8][0]=value[0][8]=value[8][8]=1.0f;
-		diamondMethod(new int[]{0,0,8,8},new int[]{0,8,0,8},3);
+		diamondMethod(new int[]{0,0,8,8},new int[]{0,8,0,8},1.0,3);
 	}
 	
-	private void squareMethod(int x, int y,int distance, int iterations){
+	private void squareMethod(int x, int y,int distance,double rand, int iterations){
 		iterations-=1;
 		value[x-distance][y]=average(value[x][y],value[x-distance][y-distance],value[x-distance][y+distance])+random(1.0);
 		value[x+distance][y]=average(value[x][y],value[x+distance][y-distance],value[x+distance][y+distance])+random(1.0);
 		value[x][y-distance]=average(value[x][y],value[x-distance][y-distance],value[x+distance][y-distance])+random(1.0);
 		value[x][y+distance]=average(value[x][y],value[x-distance][y+distance],value[x+distance][y+distance])+random(1.0);
 		if(iterations>0){
-			diamondMethod(new int[]{x-distance,x-distance,x,x},new int[]{y-distance,y,y-distance,y},iterations);
-			diamondMethod(new int[]{x-distance,x-distance,x,x},new int[]{y,y+distance,y,y+distance},iterations);
-			diamondMethod(new int[]{x,x,x+distance,x+distance},new int[]{y-distance,y,y-distance,y},iterations);
-			diamondMethod(new int[]{x,x,x+distance,x+distance},new int[]{y,y+distance,y,y+distance},iterations);
+			diamondMethod(new int[]{x-distance,x-distance,x,x},new int[]{y-distance,y,y-distance,y},rand/2,iterations);
+			diamondMethod(new int[]{x-distance,x-distance,x,x},new int[]{y,y+distance,y,y+distance},rand/2,iterations);
+			diamondMethod(new int[]{x,x,x+distance,x+distance},new int[]{y-distance,y,y-distance,y},rand/2,iterations);
+			diamondMethod(new int[]{x,x,x+distance,x+distance},new int[]{y,y+distance,y,y+distance},rand/2,iterations);
 		}
 	}
 	
-	private void diamondMethod(int[] x, int[] y,int iterations){
+	private void diamondMethod(int[] x, int[] y,double rand,int iterations){
 		int x1=(x[3]+x[0])/2;
 		int y1=(y[3]+y[0])/2;
 		value[x1][y1]=average(value[x[0]][y[0]],value[x[3]][y[0]],value[x[0]][y[3]],value[x[3]][y[3]])+random(1.0);
-		squareMethod(x1,y1,x1-x[0],iterations);
+		squareMethod(x1,y1,x1-x[0],rand/2,iterations);
 	}
 	
 	private float random(double range){
